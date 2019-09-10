d16d14d97a4b577df9e50afbaa8bdad94eeb4f72
wuhao <askwane@163.com>
2019-09-10 10:08:21 +0800

    init

---
+++ pages/chat/chat.vue
@@ -0,0 +1,112 @@
+<template>
+	<view class="chat-box">
+		<view class="chat-content">
+			<scroll-view scroll-y class="scroll-Y">
+				<view class="content" v-for="i in 20" :key="i">
+					<view class="chat-content-left uni-flex">
+						<view class="chat-icon">
+							<image src="/static/default-icon.jpg" mode=""></image>
+						</view>
+						<view class="chat-info">
+							<view class="chat-user">
+								askwan
+							</view>
+							<view class="chat-text" style="background: #fff; color: #333;">
+								hello，world都是发发发撒旦发射点发射点发生发射点发手动阀手动阀士大夫撒旦发生的首发式发生发射点发生？
+							</view>
+						</view>
+					</view>
+					<view class="chat-content-right uni-flex">
+						<view class="chat-info">
+							<view class="chat-user" style="text-align: right;">
+								askwan
+							</view>
+							<view class="chat-text">
+								hello，world？
+							</view>
+						</view>
+						<view class="chat-icon">
+							<image src="/static/default-icon.jpg" mode=""></image>
+						</view>
+					</view>
+				</view>
+				
+			</scroll-view>
+		</view>
+		<view class="chat-input uni-padding-wrap uni-flex">
+			<input class="uni-input input uni-bold" type="text" value="" />
+			<view class="center">
+				<button type="primary" size="mini">发送</button>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.chat-box{
+		$input-height:50px;
+		.center{
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+		.chat-content{
+			background: rgba(0,0,0,.1);
+			height: calc(100vh - #{$input-height});
+			overflow-y: auto;
+			.chat-info{
+				// border: 1upx solid red;
+				padding: 8rpx;
+			}
+			.chat-icon{
+				width: 60rpx;
+				height: 60rpx;
+				background-color: rgba(0,0,0,.2);
+				border-radius: 50%;
+				flex-shrink: 0;
+				overflow:hidden;
+				image{
+					width: 100%;
+					height: 100%;
+				}
+			}
+			.chat-text{
+				padding: 10rpx;
+				border-radius: 10rpx;
+				color: #fff;
+				background-color: #1AAD19;
+			}
+			.chat-content-left{
+				padding: 20upx;
+			}
+			.chat-content-right{
+				justify-content: flex-end;
+				padding: 20upx;
+			}
+		}
+		.chat-input{
+			height: $input-height;
+			// background-color: #007AFF;
+			// padding:10rpx;
+			align-items:center;
+			.input{
+				height: 30rpx;
+				padding: 8rpx 15rpx;
+				font-size: 26rpx;
+				line-height: 30rpx;
+				margin-right: 20rpx;
+				border: 2rpx solid #eee;
+			}
+		}
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,63 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			this.loadArtic()
+		},
+		methods: {
+			loadArtic(){
+				uni.request({
+					url: 'http://www.wufafuwu.com/',
+					method: 'GET',
+					data: {},
+					success: res => {
+						console.log(res,'res')
+					},
+					fail: () => {},
+					complete: () => {}
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200upx;
+		width: 200upx;
+		margin-top: 200upx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50upx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
